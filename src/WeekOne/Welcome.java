949c3dcbf1258815914546c02e26a404f5999696
shuceybb <abdurahmansuldanka@gmail.com>
2024-03-01 19:52:35 +0300

    Week One

---
+++ src/WeekOne/Welcome.java
@@ -0,0 +1,9 @@
+package WeekOne;
+import java.util.*;
+public class Welcome {
+    public static void main(String[] args) {
+    // Display the welcome message to CA222
+        System.out.println("Welcome to JAVA programming CA222");
+    }
+}
+
